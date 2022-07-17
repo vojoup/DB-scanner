023247e13412f7edd458a16ebc283d89425194c1
Vojtech Oupicky <vojtech.oupicky@showmax.com>
2022-07-17 13:19:55 +0200

    Init

---
+++ src/server/env-schema.js
@@ -0,0 +1,14 @@
+const { z } = require('zod');
+
+const envSchema = z.object({
+  DATABASE_URL: z.string().url(),
+  NODE_ENV: z.enum(['development', 'test', 'production']),
+  NEXTAUTH_SECRET: z.string(),
+  NEXTAUTH_URL: z.string().url(),
+  GOOGLE_ID: z.string(),
+  GOOGLE_SECRET: z.string(),
+  GITHUB_CLIENT_ID: z.string(),
+  GITHUB_CLIENT_SECRET: z.string(),
+});
+
+module.exports.envSchema = envSchema;
